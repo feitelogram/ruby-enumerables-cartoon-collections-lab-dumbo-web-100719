c23424d3b6709f03a71ace8e65df170a33134462
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-09-25 18:20:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,9 @@ def roll_call_dwarves (dwarves)
 end
 
 def summon_captain_planet(calls)
-  calls.map do 
+  calls.map do |call|
+    call.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
