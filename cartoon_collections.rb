b451d986bb98a68523a5f9f5837f324c010830f4
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-09-25 18:44:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,8 @@ def long_planeteer_calls(long_calls)
   long_calls.any? { |call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.include?
 end
